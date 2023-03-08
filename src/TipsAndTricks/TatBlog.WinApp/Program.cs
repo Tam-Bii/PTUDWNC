bc5173b48547b55f842b303ee97c97a23c80aa91
Tam-Bii <tambii.tb@gmail.com>
2023-03-09 02:27:41 +0700

    Xây dựng các lớp để truy vấn dữ liệu

--- src/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,19 +1,74 @@
 ﻿using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
 
 
-//Tạo đối tượng DBContext để quản lý phiên làm việc
+////Tạo đối tượng DBContext để quản lý phiên làm việc
+//// với CSDL và trạng thái của đối tượng
+//var context = new BlogDbContext();
+//// Tạo đối tượng khởi tạo dữ liệu mẫu
+//var seeders = new DataSeeder(context);
+//// Gọi hàm initialize để nhập dữ liệu mẫu
+//seeders.Initialize();
+//// đọc danh sách tác giả từ csdl
+//var authors = context.Authors.ToList();
+//// Xuất danh sách tác giả ra màn hình
+//Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}", "ID", "Full Name", "Email", "Joined Date");
+//foreach (var author in authors)
+//{
+//    Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}", author.Id, author.FullName, author.Email, author.JoinedDate);
+//}
+
+
+//------để đọc và hiển thị danh sách bài viết kèm theo tên chuyên mục, tác giả.-----
+//// Tạo đối tượng DBContext để quản lý phiên làm việc
+//// với CSDL và trạng thái của đối tượng
+//var context = new BlogDbContext();
+//// Đọc danh sách bài viết từ CSDL
+//// Lấy kém tên tác giả và chuyên mục
+//var posts = context.Posts
+//    .Where(p => p.Published)
+//    .OrderBy(p => p.Title)
+//    .Select(p => new
+//    {
+//        Id = p.Id,
+//        Title = p.Title,
+//        ViewCount = p.ViewCount,
+//        PostedDate = p.PostedDate,
+//        Author = p.Author.FullName,
+//        Category = p.Category.Name
+//    })
+//    .ToList();
+////Xuất danh sách bài viết ra màn hình
+//foreach (var post in posts)
+//{
+//    Console.WriteLine("ID               : {0}", post.Id);
+//    Console.WriteLine("Title            : {0}", post.Title);
+//    Console.WriteLine("ViewCount        : {0}", post.ViewCount);
+//    Console.WriteLine("Date             : {0:MM/dd/yyyy}", post.PostedDate);
+//    Console.WriteLine("Author           : {0}", post.Author);
+//    Console.WriteLine("Category         : {0}", post.Category);
+//    Console.WriteLine("".PadRight(80, '-'));
+//}
+
+
+//-----để xem hoạt động của các phương thức vừa định nghĩa----
+// Tạo đối tượng DBContext để quản lý phiên làm việc
 // với CSDL và trạng thái của đối tượng
 var context = new BlogDbContext();
-// Tạo đối tượng khởi tạo dữ liệu mẫu
-var seeders = new DataSeeder(context);
-// Gọi hàm initialize để nhập dữ liệu mẫu
-seeders.Initialize();
-// đọc danh sách tác giả từ csdl
-var authors = context.Authors.ToList();
-// Xuất danh sách tác giả ra màn hình
-Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}", "ID", "Full Name", "Email", "Joined Date");
-foreach (var author in authors)
+// Tạo đối tượng BlogRepository
+IBlogRepository blogRepo = new BlogRepository(context);
+// Tìm 3 bài viết được xem/đọc nhiều nhất
+var posts = await blogRepo.GetPopularArticlesAsync(3);
+
+// Xuất danh sách bài viết ra màn hình
+foreach (var post in posts)
 {
-    Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}", author.Id, author.FullName, author.Email, author.JoinedDate);
+    Console.WriteLine("ID               : {0}", post.Id);
+    Console.WriteLine("Title            : {0}", post.Title);
+    Console.WriteLine("ViewCount        : {0}", post.ViewCount);
+    Console.WriteLine("Date             : {0:MM/dd/yyyy}", post.PostedDate);
+    Console.WriteLine("Author           : {0}", post.Author);
+    Console.WriteLine("Category         : {0}", post.Category);
+    Console.WriteLine("".PadRight(80, '-'));
 }
