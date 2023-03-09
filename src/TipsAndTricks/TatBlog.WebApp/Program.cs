7f6965e527c7c2c87ff17ccda1a488196f282ea0
Tam-Bii <tambii.tb@gmail.com>
2023-03-09 18:04:48 +0700

    Cấu hình chuỗi kết nối CSDL và cập nhật lớp DbContext, Program

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllersWithViews();
+
+    builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
 }
 var app = builder.Build();
 {
@@ -28,3 +40,12 @@ var app = builder.Build();
         pattern: "{controller=Blog}/{action=Index}/{id?}");
 }
 app.Run();
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetService<IDataSeeder>(); ;
+    seeder.Initialize();
+}
+
+app.Run();
+
