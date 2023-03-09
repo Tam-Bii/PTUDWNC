dfb9cf042a2e928f78dbe38e7a4ed3ab95133dd1
Tam-Bii <tambii.tb@gmail.com>
2023-03-09 18:49:23 +0700

    Tạo thanh điều hướng trang (Pager)

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -38,13 +38,32 @@ var app = builder.Build();
     app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Blog}/{action=Index}/{id?}");
+
+    app.MapControllerRoute(
+        name: "posts-by-category",
+        pattern: "blog/category/{slug}",
+        defaults: new { controller = "Blog", action = "Category" });
+
+    app.MapControllerRoute(
+       name: "posts-by-tag",
+       pattern: "blog/tag/{slug}",
+       defaults: new { controller = "Blog", action = "Tag" });
+
+    app.MapControllerRoute(
+       name: "single-post",
+       pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+       defaults: new { controller = "Blog", action = "Post" });
+
+    app.MapControllerRoute(
+       name: "default",
+       pattern: "{controller=Blog}/{action=Index}/{id?}");
 }
 app.Run();
 
 using (var scope = app.Services.CreateScope())
 {
     var seeder = scope.ServiceProvider.GetService<IDataSeeder>(); 
-    seeder?.Initialize();
+    seeder.Initialize();
 }
 
 app.Run();
